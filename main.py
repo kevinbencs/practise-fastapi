9b69543ab93b239e8b47a0e75f58ba29ed5b0f53
B.Kev <b.kev@domain.com>
2026-05-19 12:33:09 +0200

    Added HTTP basic auth.

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, Depends
 import jwt
 from pwdlib import PasswordHash
 from jwt.exceptions import InvalidTokenError
+from typing import Annotated
+from fastapi.security import HTTPBasic, HTTPBasicCredentails
+
 
 app = FastAPI()
+
+security = HTTPBasic()
 
 @app.get("/")
 def reat_root(status_code: 200):
     return {"message": "Main route"}
 
 
-@app.post("/register")
-def register(email: str, password: str, status_code: 201):
-    return {"message": "success"}
-
-@app.post("/login")
-def login(email: str, password: str, response: Response, status_code: 200):
-
-    response.set_cookie(key="authsession", value="token", secure=True, httponly=True)
-    return {"message": "success"}
+@app.get("/user/me")
+def get_user(credentails: Annotated[HTTPBasicCredentails, Depends(security)]):
+    return {"username": credentails.username, "pssword": credentails.password}
 
 
-@app.get("/logout")
-def logout(response: Response, status_code: 200):
-    response.delete_cookie(key="authsession", secure=True, httponly=True)
-    return {"message": "success"}
+
+#@app.post("/register")
+#def register(email: str, password: str, status_code: 201):
+#    return {"message": "success"}
+#
+#@app.post("/login")
+#def login(email: str, password: str, response: Response, status_code: 200):
+#
+#    response.set_cookie(key="authsession", value="token", secure=True, httponly=True)
+#    return {"message": "success"}
+#
+#
+#@app.get("/logout")
+#def logout(response: Response, status_code: 200):
+#    response.delete_cookie(key="authsession", secure=True, httponly=True)
+#    return {"message": "success"}
