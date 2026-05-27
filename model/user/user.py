14b775afd797f87520f100f8c0b9fd681130c7d6
B.Kev <b.kev@domain.com>
2026-05-27 13:07:44 +0200

    Created user model

---
+++ model/user/user.py
@@ -0,0 +1,12 @@
+from sqlmodel import Field, Session, SQLModel, create_engine, select, Relationship
+from typing import List
+from app.model.book.book import Book
+
+
+
+class User(SQLModel, table=True):
+    id: int | None=Field(default = None, primary_key = True)
+    name:  str = Field(index =True)
+    email: str= Field(index = True)
+    password: str =Field(index =True)
+    Bookes: List[Book] = Relationship(back_populates="user")
