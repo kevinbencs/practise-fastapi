6c97c2f41342f5834ef994d78dc1969124acf40f
B.Kev <b.kev@domain.com>
2026-06-02 14:31:37 +0200

    Change sqlite to postgresSQL

--- app/db.py
+++ app/db.py
@@ -1,14 +1,16 @@
 from sqlmodel import Field, Session, SQLModel, create_engine
 from typing import Annotated
 from fastapi import Depends
+import os
 
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+##sqlite_file_name = "database.db"
 
+##sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-connect_args = {"check_same_thread":False}
-engine = create_engine(sqlite_url, connect_args=connect_args)
-
+DB_URL = os.getenv("DTABASE_URL", "postgresql:user:....")
+##connect_args = {"check_same_thread":False}
+##engine = create_engine(sqlite_url, connect_args=connect_args)
+engine = create_engine(DB_URL)
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
